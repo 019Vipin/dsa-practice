7245c2a7bafad5ed1762e495562a2dfc2444e0fb
Vipin <157127686+019Vipin@users.noreply.github.com>
2026-08-09 15:17:26 +0530

    Time: 50 ms (36.17%) | Memory: 121.3 MB (11.70%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<int>rhm(n,0);
+        int rm=0;
+        int i=n-1;
+        int j=m-1;
+        while(i>=0){
+            if(j>=0 && word1[i]==word2[j]){
+                rm++;
+                j--;
+            }
+            rhm[i]=rm;
+            i--;
+        }
+        vector<int>seq;
+        bool chg_pow=true;
+        i=0;
+        j=0;
+        while(i<n && j<m){
+            if(word1[i]==word2[j]){
+                seq.push_back(i);
+                j++;
+            } else if(chg_pow==true && i+1<n && rhm[i+1]>=m-j-1){
+                seq.push_back(i);
+                j++;
+                chg_pow=false;
+            }
+            i++;
+        }
+        return j==m?seq:vector<int>();
+    }
+};
